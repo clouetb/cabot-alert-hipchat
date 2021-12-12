110885b8c705dd7f38f0729698fc355abbe209ab
Benoît Clouet <benoit.clouet@gmail.com>
2021-12-12 15:39:55 +0100

    Set on_delete strategy.

--- cabot_alert_hipchat/migrations/0001_initial.py
+++ cabot_alert_hipchat/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='HipchatAlert',
             fields=[
-                ('alertplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
+                ('alertplugin_ptr', models.OneToOneField(parent_link=True, on_delete=models.CASCADE, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
             ],
             options={
                 'abstract': False,
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='HipchatAlertUserData',
             fields=[
-                ('alertpluginuserdata_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPluginUserData')),
+                ('alertpluginuserdata_ptr', models.OneToOneField(parent_link=True, on_delete=models.CASCADE, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPluginUserData')),
                 ('hipchat_alias', models.CharField(max_length=50, blank=True)),
             ],
             options={
